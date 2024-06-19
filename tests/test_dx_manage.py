472d78d8aa34eb61684442829e8b4d8b4a44114e
Jethro Rainford <rainford1995@gmail.com>
2024-06-19 14:34:14 +0100

    add tests for dx_manage.unarchive_files

--- tests/test_dx_manage.py
+++ tests/test_dx_manage.py
@@ -13,9 +13,147 @@ class TestCheckArchivalState(unittest.TestCase):
 
 
 class TestUnarchiveFiles(unittest.TestCase):
-    """ """
+    """
+    Tests for dx_manage.unarchive_files()
 
-    pass
+    Function called by dx_manage.check_archival_state where one or more
+    archived files found and unarchive=True set, will go through the
+    given file IDs and start the unarchiving process
+    """
+    # minimal dxpy.find_data_objects() return that we expect to unarchive
+    files = {
+        'project-xxx': [
+            {
+                'project': 'project-xxx',
+                'id': 'file-xxx',
+                'describe': {
+                    'name': 'sample1-file1',
+                    'archivalState': 'archived'
+                }
+            },
+            {
+                'project': 'project-xxx',
+                'id': 'file-yyy',
+                'describe': {
+                    'name': 'sample2-file1',
+                    'archivalState': 'archived'
+                }
+            }
+        ]
+    }
+
+
+    @pytest.fixture(autouse=True)
+    def capsys(self, capsys):
+        """Capture stdout to provide it to tests"""
+        self.capsys = capsys
+
+
+    @patch('bin.utils.dx_manage.dxpy.api.project_unarchive')
+    @patch('bin.utils.dx_manage.exit')
+    def test_unarchiving_called(
+            self,
+            exit,
+            mock_unarchive
+        ):
+        """
+        Test that dxpy.api.project_unarchive() gets called on
+        the provided list of DXFile objects
+        """
+        dx_manage.unarchive_files(self.files)
+
+        mock_unarchive.assert_called()
+
+
+    @patch('bin.utils.dx_manage.dxpy.api.project_unarchive')
+    @patch('bin.utils.dx_manage.exit')
+    def test_unarchive_called_per_project(
+            self,
+            exit,
+            mock_unarchive
+        ):
+        """
+        If files found are in more than one project the function
+        will loop over each set of files per project, test that this
+        correctly happens where files are in 3 projects
+        """
+        # minimal example of 3 files in 3 separate projects
+        files = {
+            'project-xxx': [
+                {
+                    'project': 'project-xxx',
+                    'id': 'file-xxx',
+                    'describe': {
+                        'name': 'sample1-file1',
+                        'archivalState': 'archived'
+                    }
+                }
+            ],
+            'project-yyy': [
+                {
+                    'project': 'project-yyy',
+                    'id': 'file-yyy',
+                    'describe': {
+                        'name': 'sample2-file1',
+                        'archivalState': 'archived'
+                    }
+                }
+            ],
+            'project-zzz': [
+                {
+                    'project': 'project-zzz',
+                    'id': 'file-zzz',
+                    'describe': {
+                        'name': 'sample2-file1',
+                        'archivalState': 'archived'
+                    }
+                }
+            ]
+        }
+
+        dx_manage.unarchive_files(files)
+
+        self.assertEqual(mock_unarchive.call_count, 3)
+
+
+    @patch(
+        'bin.utils.dx_manage.dxpy.api.project_unarchive',
+        side_effect=Exception('someDNAnexusAPIError')
+    )
+    def test_error_raised_if_unable_to_unarchive(
+            self,
+            mock_unarchive
+        ):
+        """
+        If any error is raised during calling dxpy.api.project_unarchive
+        it will be caught and raise a RuntimeError, test that if an
+        Exception is raised we get the expected error message
+        """
+        with pytest.raises(
+            RuntimeError,
+            match='Error unarchiving files'
+        ):
+            dx_manage.unarchive_files(self.files)
+
+
+    @patch('bin.utils.dx_manage.dxpy.api.project_unarchive')
+    @patch('bin.utils.dx_manage.exit')
+    def test_check_state_command_correct(self, exit, mock_unarchive):
+        """
+        Test that when the function calls all the unarchiving, that
+        the message printed to stdout with a command to check the state
+        of all files is correct
+        """
+        dx_manage.unarchive_files(self.files)
+
+        expected_stdout = (
+            "echo file-xxx file-yyy | xargs -n1 -d' ' -P32 -I{} bash -c 'dx "
+            "describe --json {} ' | grep archival | uniq -c"
+        )
+
+        assert expected_stdout in self.capsys.readouterr().out, (
+            "check state command not as expected"
+        )
 
 
 class TestCreateFolder(unittest.TestCase):
