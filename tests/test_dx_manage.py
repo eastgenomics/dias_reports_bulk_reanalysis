f4c35a545823de6ab67b2cf7e268b6ddebb49696
Jethro Rainford <rainford1995@gmail.com>
2024-06-19 11:36:40 +0100

    add test.dx_manage framework

---
+++ tests/test_dx_manage.py
@@ -0,0 +1,110 @@
+import unittest
+
+
+from bin.utils import dx_manage
+
+
+class TestCheckArchivalState(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestUnarchiveFiles(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestCreateFolder(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestFindInParallel(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetCnvCallJob(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetDependentFiles(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetJobStates(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetLaunchedWorkflowIds(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetProjects(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetXlsxReports(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetSingleDir(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestGetLatestDiasBatchApp(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestRunBatch(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+class TestReadGenepanelsFile(unittest.TestCase):
+    """
+
+    """
+    pass
+
+
+
+class TestUploadManifest(unittest.TestCase):
+    """
+
+    """
+    pass
